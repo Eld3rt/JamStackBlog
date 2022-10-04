fd1c4bf4b9b77b656dc3241b2e6fae69e506fd2c
Eld3rt <kirrey8ynwa@gmail.com>
2022-10-04 13:07:27 +0300

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,5 @@ import CMS from 'netlify-cms-app';
 import { Figure } from 'netlify-cms-editor-component-figure';
 // Initialize the CMS object
 CMS.init();
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate('my-template', MyTemplate);
 
 CMS.registerEditorComponent(Figure);
