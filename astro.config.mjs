55e9a5b207f200567d1fb431094929ac9ac24cd5
Eld3rt <kirrey8ynwa@gmail.com>
2022-10-04 12:01:54 +0300

    Integrate Netlify CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,6 @@
-import { defineConfig } from 'astro/config';
-import NetlifyCMS from 'astro-netlify-cms';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: 'git-gateway',
-          branch: 'main',
-        },
-        media_folder: 'public/assets/img',
-        public_folder: '/assets/img',
-        collections: [
-          {
-            name: 'blog',
-            label: 'Blog',
-            folder: 'src/pages/blog',
-            create: true,
-						slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
-            fields: [
-							{ label: "Layout", name: 'layout', widget: "hidden", default: "../../layouts/BlogPostLayout.astro" },
-              { label: "Title", name: 'title', widget: "string" },
-							{ label: "Description", name: "description", widget: "string" },
-							{ label: "Author", name: "author", widget: "string" },
-              { label: 'Date', name: 'date', widget: 'datetime', format: 'MMMM d, Y', date_format: 'MMMM d, Y', time_format: false },
-							{ label: "Featured Image", name: "image", widget: "image" },
-							{ label: "Image caption", name: "imageAlt", widget: "string" },
-							{ label: "Tags", name: "tags", widget: "list", default: "post" },
-							{ label: "Body", name: "body", widget: "markdown" },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
-});
+  // your configuration options here...
+  // https://docs.astro.build/en/reference/configuration-reference/
+})
